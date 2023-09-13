4d6a22b4d1022e6386863834904ecd832f36a33d
SeanMcAvoy29 <sean.mcavoy@Kainos.com>
2023-09-13 11:38:33 +0100

    Job Spec DAO done

---
+++ src/main/java/org/kainos/ea/db/JobspecDAO.java
@@ -0,0 +1,32 @@
+package org.kainos.ea.db;
+
+import org.kainos.ea.cli.Job;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JobspecDAO {
+
+    private DatabaseConnector databaseConnector = new DatabaseConnector();
+
+    public Job getJobspec(int id) throws SQLException {
+
+        Connection c = databaseConnector.getConnection();
+        Statement st = c.createStatement();
+
+        ResultSet rs = st.executeQuery("SELECT * FROM JobRole where JobID =" +id);
+
+        while(rs.next()){
+            return new Job(
+                    rs.getInt("JobID"),
+                    rs.getString("JobRole"),
+                    rs.getString("Band"),
+                    rs.getString("Specifications"),
+                    rs.getString("Responsibilities")
+            );
+        }
+        return null;
+    }
+}
