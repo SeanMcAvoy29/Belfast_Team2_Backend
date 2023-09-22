4cbc6ee601ce2d924b311f5079833025ff133b4a
Joel McConvey <joel.mcconvey@kainos.com>
2023-09-22 12:10:55 +0100

    Updated checktyle.xml file and fixed errors found

--- src/main/java/org/kainos/ea/DropwizardApplicationConfiguration.java
+++ src/main/java/org/kainos/ea/DropwizardApplicationConfiguration.java
@@ -1,12 +1,6 @@
 package org.kainos.ea;
 
 import io.dropwizard.Configuration;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
-import org.hibernate.validator.constraints.*;
-
-import javax.validation.Valid;
-import javax.validation.constraints.*;
 
 public class DropwizardApplicationConfiguration extends Configuration {
     // TODO: implement service configuration
